a71c3b1712a3ca186bb02ae56dd67cd709d10238
Prabesh Kumar Bhagat <pb610335@indbusiness.dc>
2023-02-14 08:06:32 +0530

    made changes as per reviewers comment

--- src/main/java/SeleniumSessions/cartpage.java
+++ src/main/java/SeleniumSessions/cartpage.java
@@ -1,6 +1,19 @@
 package SeleniumSessions;
 
+import org.openqa.selenium.By;
+
 public class cartpage {
+
+	By cart = By.id("cartpagelocator");
+
+	public cartpage(By cart) {
+		super();
+		this.cart = cart;
+	}
+	
+	public void addToCart() {
+		
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
