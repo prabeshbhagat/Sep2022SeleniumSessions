7beb701396540eda67350f94f350c5a27b3a51b9
Prabesh Kuamr Bhagar <35034526+prabeshbhagat@users.noreply.github.com>
2023-03-12 15:23:51 +0530

    Create conflictTest.java

---
+++ src/main/java/SeleniumSessions/conflictTest.java
@@ -0,0 +1,10 @@
+package SeleniumSessions;
+
+public class conflictTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("conflictTest--prabesh");
+	}
+
+}
