ed427c9e1bec88ad2f9d460a69c2f90e5d0f43a1
Prabesh Kumar Bhagat <pb610335@indbusiness.dc>
2023-03-12 12:15:20 +0530

    test branch code

---
+++ src/main/java/SeleniumSessions/bugfix.java
@@ -0,0 +1,12 @@
+package SeleniumSessions;
+
+public class bugfix {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Bugfix");
+
+	}
+
+}
